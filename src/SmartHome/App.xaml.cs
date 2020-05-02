390c29d758c912086ab1b063c932f881f95c6b05
FeOkuma <feokuma@gmail.com>
2020-05-02 19:31:03 -0300

    Criação do projeto com prism

---
+++ src/SmartHome/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using SmartHome.ViewModels;
+using SmartHome.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHome
+{
+    public partial class App : PrismApplication
+    {
+        public App() : base(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer, true) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("NavigationPage/MainPageView");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPageView, MainPageViewModel>();
+        }
+    }
+}
---
+++ src/SmartHome/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SmartHome.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private readonly INavigationService navigationService;
+
+        public ICommand NextPageCommand { get; }
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            NextPageCommand = new Command(NextPage);
+            this.navigationService = navigationService;
+        }
+
+        public async void NextPage()
+        {
+            await DisplayAlert("Alert", "You have been alerted", "Ok");
+        }
+    }
+}
