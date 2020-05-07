5db69f2bfe140ac3be481523276a3a730de9d365
FeOkuma <feokuma@gmail.com>
2020-05-06 21:40:25 -0300

    Separa o card em um componente e adiciona skia sharp no projeto

---
+++ src/SmartHome/Controls/CardRoom.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartHome.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardRoom : ContentView
+    {
+        public CardRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void canvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            using (SKPaint paint = new SKPaint())
+            {
+                // Create gradient for background
+                paint.Shader = SKShader.CreateLinearGradient(
+                                    new SKPoint(0, 0),
+                                    new SKPoint(info.Width, info.Height),
+                                    new SKColor[] { new SKColor(0x7a, 0x53, 0xcb),
+                                                new SKColor(0xf5, 0x42, 0x8d) },
+                                    null,
+                                    SKShaderTileMode.Clamp);
+
+                // Draw background
+                canvas.DrawRect(info.Rect, paint);
+            }
+        }
+    }
+}
--- src/SmartHome/ViewModels/MainPageViewModel.cs
+++ src/SmartHome/ViewModels/MainPageViewModel.cs
@@ -13,18 +13,9 @@ namespace SmartHome.ViewModels
     {
         private readonly INavigationService navigationService;
 
-        public ICommand NextPageCommand { get; }
-        public ObservableRangeCollection<string> textos { get; set; }
         public MainPageViewModel(INavigationService navigationService)
         {
-            NextPageCommand = new Command(NextPage);
             this.navigationService = navigationService;
-            textos = new ObservableRangeCollection<string>();
-            textos.AddRange(new string[] { "Texto 1", "Texto 2", "Texto 3"});
-        }
-
-        public async void NextPage()
-        {
         }
     }
 }
