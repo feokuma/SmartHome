8a14e3958687a7c3d1bd99172a7186ddedfbcf54
FeOkuma <feokuma@gmail.com>
2020-05-28 23:07:19 -0300

    Adiciona circulo colorido no detalhe do ambiente

--- src/SmartHome/Views/MainPageView.xaml.cs
+++ src/SmartHome/Views/MainPageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +17,32 @@ namespace SmartHome.Views
         public MainPageView()
         {
             InitializeComponent();
+        }
+
+        private void skiaCanvas_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs args)
+        {
+            var imageInfo = args.Info;
+            var surface = args.Surface;
+            var canvas = surface.Canvas;
+            var initialColor = new SKColor(0x7a, 0x53, 0xcb);
+            var finalColor = new SKColor(0xf5, 0x42, 0x8d);
+
+            canvas.Clear();
+
+            using (SKPaint paint = new SKPaint())
+            {
+                // Create gradient for background
+                paint.Shader = SKShader.CreateLinearGradient(
+                                    new SKPoint(0, 0),
+                                    new SKPoint(imageInfo.Width/2, imageInfo.Height/2),
+                                    new SKColor[] { initialColor, finalColor },
+                                    null,
+                                    SKShaderTileMode.Clamp);
+
+                var point = new SKPoint(imageInfo.Width/2,imageInfo.Height/2);
+                // Draw background
+                canvas.DrawCircle(point,imageInfo.Width/2, paint);
+            }
         }
     }
 }
